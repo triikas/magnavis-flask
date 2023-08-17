a2abd2805c5c54a0e187485f99fa0d32a978edf4
TriPC <triikas@triikas.ru>
2023-08-17 21:28:40 +0300

    17.08 2

--- static/func.js
+++ static/func.js
@@ -1,6 +1,33 @@
-const items = document.querySelectorAll(".accordion a");
-function toggleAccordion(){
-  this.classList.toggle('active');
-  this.nextElementSibling.classList.toggle('active');
-}
-items.forEach(item => item.addEventListener('click', toggleAccordion));
+// const items = document.querySelectorAll(".accordion a");
+// function toggleAccordion(){
+//   this.classList.toggle('active');
+//   this.nextElementSibling.classList.toggle('active');
+// }
+// items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+$(function() {
+  var $speed = 200;
+  var $class = 'opened';
+  var $class_open = '.faq-answer';
+
+  $(document).ready(function() {
+    $('.faq-question').on('click', function() {
+      $ul = $(this).closest('ul');
+      $answer = $(this).closest('li').find($class_open);
+
+      if (!$(this).closest('li').hasClass($class)) {
+
+        $ul.find('li').each(function() {
+          if ($(this).hasClass($class))
+            $(this).removeClass($class).find($class_open).slideUp($speed);
+        });
+      }
+
+      $answer
+        .slideToggle($speed)
+        .closest('li')
+        .toggleClass($class);
+    });
+  });
+});
