06855bc15eac6269e6a8412183e2abd87986f9a5
Office 3 <q1113p@mail.ru>
2023-08-31 13:26:04 +0300

    p-up down

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/contacts')
+@application.route('/contacts')
 def contacts():
     return render_template('contacts.html')
 
-@app.route('/payment')
+@application.route('/payment')
 def payment():
     return render_template('payment.html')
 
-@app.route('/services')
+@application.route('/services')
 def services():
     return render_template('services.html')
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', debug=False)
